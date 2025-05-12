9ca879e869aaed312b926c5d9effa28cfae4bd1d
Guilherme Rocha <guilhermepmrocha@gmail.com>
2025-05-12 00:26:15 -0300

    wip

--- application/app/Http/Controllers/LoginController.php
+++ application/app/Http/Controllers/LoginController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function index() : View
     {
         return view('login.index');
     }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        dd($credentials);
+    }
+    
 }
--- application/resources/views/login/index.blade.php
+++ application/resources/views/login/index.blade.php
@@ -11,7 +11,7 @@
     <div class="flex items-center justify-center min-h-screen">
         <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
             <h2 class="text-2xl font-bold mb-6 text-center">Login </h2>
-            <form action="#" method="POST">
+            <form action="{{ route('login.store') }}" method="POST">
                 <div class="mb-4">
                     <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                     <input type="email" id="email" name="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
